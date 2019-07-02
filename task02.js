3117ca89dac86d14b79d5cc80d27cf2165d48896
Lyudmyla Lyashuk <lyudmyla.horbyk@gmail.com>
2019-07-02 23:03:47 +0300

    Update task02.js

--- task02.js
+++ task02.js
@@ -1,6 +1,6 @@
 // Task 02 - https://www.codewars.com/kata/training-js-number-2-basic-data-types-number/train/javascript
 
-var v1 = 50,v2 = 100,v3 = 150,v4 = 200,v5 = 2,v6 = 250
+var v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250
 function equal1() {
   var a = v1   
   var b = v1   
@@ -29,5 +29,5 @@ function equal4() {
 function equal5() {
   var a = v6   //set number value to a
   var b = v3   //set number value to b
-  return a%b;
+  return a % b;
 }
